3f0884218d2fdff6621c0d2512e6526f057cae1f
Chris Christensen <christianchristensen@gmail.com>
2013-04-23 00:46:54 -0500

    Add geben debugger autoloader and some cleanup.

--- init.el
+++ init.el
@@ -2,7 +2,6 @@
 (transient-mark-mode 1) ; highlight text selection
 (delete-selection-mode 1) ; delete seleted text when typing
 (show-paren-mode 1) ; turn on paren match highlighting
-; (setq show-paren-style 'expression) ; highlight entire bracket expression
 (setq make-backup-files nil) ; stop creating ~ files
 (line-number-mode 1) ; always show line numbers
 (column-number-mode 1) ; " column "
@@ -19,3 +18,4 @@
 (require 'git-commit)
 (add-hook 'git-commit-mode-hook 'turn-on-flyspell)
 (add-hook 'git-commit-mode-hook (lambda () (toggle-save-place 0)))
+(autoload 'geben "geben" "PHP Debugger on Emacs" t)
